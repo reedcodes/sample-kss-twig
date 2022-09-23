653f9660eabca6e82dc44b5331a831b34cea1041
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2022-09-23 12:47:51 -0400

    move homepage, config files

---
+++ config/color-tokens.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: "primary",
+    color: "hsla(60, 1%, 14%, 1)",
+  },
+  {
+    name: "secondary",
+    color: "hsla(44, 88%, 66%, 1)",
+  },
+  {
+    name: "tertiary",
+    color: "hsla(150, 14%, 84%, 1)",
+  },
+  {
+    name: "background",
+    color: "hsla(81, 28%, 90%, 1)"
+  },
+  {
+    name: "foreground",
+    color: "hsla(120, 2%, 20%, 1)"
+  }
+];
